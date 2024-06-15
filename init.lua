1999b27d19ddfefd787bc9ea97c9ca3d893f4dcf
sulthonuladib <adibcyber4rt@gmail.com>
2024-06-15 16:01:31 +0700

    separate this from dotfiles

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("sulthonuladib")
+-- vim.cmd[[colorscheme tokyonight-night]]
+-- vim.notify = require("notify")
+
+vim.cmd.colorscheme("tokyonight-storm")
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000", border = "orange" }
---
+++ lua/sulthonuladib/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- dependencies = {
+		-- 	{ dir = "~/plugins/tree-sitter-lua" },
+		-- },
+		build = ":TSUpdate",
+		-- branch = "main",
+		-- lazy = false,
+		config = function()
+			require("sulthonuladib.configs.treesitter").setup()
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+}
